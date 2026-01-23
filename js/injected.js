1854aad8a8a3bf577c14d8202d25328d5e8ab982
seangels <sang.nq101090@gmail.com>
2026-01-23 18:16:17 +0700

    add log to track inject

--- js/injected.js
+++ js/injected.js
@@ -1,29 +1,34 @@
-window.__env = {
-  production: false,
-  apiBaseUrl: "",
-  currentUser: {
-    userId: window.__ctx?.email,
-    userName: window.__ctx?.email,
-    accessCode: "",
-    fullName: window.__ctx?.email,
-    email: window.__ctx?.email,
-    permissions: [
-      { code: "change_request__grid__row-onApproveClick" },
-      { code: "change_request__grid__row-onDeleteButtonClick" },
-      { code: "change_request__grid__row-onEditButtonClick" },
-      { code: "change_request__grid__row-onRejectClick" },
-      { code: "change_request__grid__row-onReopenClick" },
-      { code: "change_request__grid__toolbarItems-onTaoYC_sua_xoa_them" },
-      { code: "change_request__popup-onSaveClick" },
-      { code: "change_request__grid__toolbarItems-onTaoYC" },
-    ],
-  },
-  initData: {
-    newChangeRequestData: {
-      contextKey: "phieu-xuat",
-      changeType: "create",
-      objectRowIndexsString: "18",
-      objectIdsString: "PX-004",
+(function () {
+  console.log("start injected.");
+  window.__env = {
+    production: false,
+    apiBaseUrl: "",
+    currentUser: {
+      userId: window.__ctx?.email,
+      userName: window.__ctx?.email,
+      accessCode: "",
+      fullName: window.__ctx?.email,
+      email: window.__ctx?.email,
+      permissions: [
+        { code: "change_request__grid__row-onApproveClick" },
+        { code: "change_request__grid__row-onDeleteButtonClick" },
+        { code: "change_request__grid__row-onEditButtonClick" },
+        { code: "change_request__grid__row-onRejectClick" },
+        { code: "change_request__grid__row-onReopenClick" },
+        { code: "change_request__grid__toolbarItems-onTaoYC_sua_xoa_them" },
+        { code: "change_request__popup-onSaveClick" },
+        { code: "change_request__grid__toolbarItems-onTaoYC" },
+      ],
     },
-  },
-};
+    initData: {
+      newChangeRequestData: {
+        contextKey: "phieu-xuat",
+        changeType: "create",
+        objectRowIndexsString: "18",
+        objectIdsString: "PX-004",
+      },
+    },
+  };
+
+  console.log("end injected.");
+})();
