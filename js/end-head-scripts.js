ed6620a3207b834c9bc7c6c622dafc7b6935f61f
seangels <sang.nq101090@gmail.com>
2026-01-24 21:39:58 +0700

    inject env with object

--- js/end-head-scripts.js
+++ js/end-head-scripts.js
@@ -3,7 +3,7 @@
   window.__env = {
     production: false,
     apiBaseUrl: "",
-    currentUser: JSON.stringify({
+    currentUser: {
       userId: window.__ctx?.email,
       userName: window.__ctx?.email,
       accessCode: "",
@@ -19,15 +19,15 @@
         { code: "change_request__popup-onSaveClick" },
         { code: "change_request__grid__toolbarItems-onTaoYC" },
       ],
-    }),
-    initData: JSON.stringify({
+    },
+    initData: {
       newChangeRequestData: {
         contextKey: "phieu-xuat",
         changeType: "create",
         objectRowIndexsString: "18",
         objectIdsString: "PX-004",
       },
-    }),
+    },
   };
 
   console.log("end injected.");
